ec531b3fa2e851e2c51a7503068c42b65ff6fafd
Md. Faishal Ahmed <111577729+faishalAhmed03@users.noreply.github.com>
2023-06-19 00:41:51 +0600

    TFLite_detection_webcam.py

--- TFLite_detection_webcam.py
+++ TFLite_detection_webcam.py
@@ -1,7 +1,4 @@
 ######## Webcam Object Detection Using Tensorflow-trained Classifier #########
-#
-# Author: Evan Juras
-# Date: 10/27/19
 # Description: 
 # This program uses a TensorFlow Lite model to perform object detection on a live webcam
 # feed. It draws boxes and scores around the objects of interest in each frame from the
@@ -10,8 +7,6 @@
 #
 # This code is based off the TensorFlow Lite image classification example at:
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/examples/python/label_image.py
-#
-# I added my own method of drawing boxes and labels using OpenCV.
 
 # Import packages
 import os
